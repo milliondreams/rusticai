df6b0a7c8f63e70bd6aafbbcc8d8113ad1c29f44
Rohit Rai <inbox@rohitrai.com>
2023-08-07 20:54:45 -0700

    adding basic methods to server

--- rustic_ai/server/main.py
+++ rustic_ai/server/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from rustic_ai import __version__
+
 app = FastAPI()
 
 
@@ -7,3 +9,17 @@ app = FastAPI()
 async def root():
     """Root endpoint for the API."""
     return {"message": "Hello World"}
+
+
+@app.get("/health")
+async def health():
+    """Health endpoint for the API."""
+    return {"message": "OK"}
+
+
+@app.get("/version")
+async def version():
+    """Version endpoint for the API."""
+    # Get version of the current package
+    version = __version__
+    return {"version": version}
--- tests/server/test_server.py
+++ tests/server/test_server.py
@@ -2,6 +2,8 @@ import unittest
 
 from fastapi.testclient import TestClient
 
+from rustic_ai import __version__ as version
+
 
 class TestServer(unittest.TestCase):
     def setUp(self):
@@ -14,3 +16,13 @@ class TestServer(unittest.TestCase):
     def test_returns_message_key(self):
         response = self.client.get('/')
         assert 'message' in response.json().keys()
+
+    # Test health check returns a JSON with 'message' value 'OK'
+    def test_health_check(self):
+        response = self.client.get('/health')
+        assert response.json()['message'] == 'OK'
+
+    # Test version check returns a JSON with 'version' of the package
+    def test_version_check(self):
+        response = self.client.get('/version')
+        assert response.json()['version'] == version
